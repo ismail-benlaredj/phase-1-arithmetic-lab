ef0472e703dcb3eea9b1a6f90ced10e3de3d0055
ismail <yako_is@yahoo.fr>
2023-07-23 15:09:14 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 3.875;
+let num2 = 16
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 100) + 1
+
+let num3 = 44;
+let num4 = 40;
+let mod = num3 % num4;
+
+let max = Math.max(20, 1, 2, 3, 4, 5, 6, 8, 20);
